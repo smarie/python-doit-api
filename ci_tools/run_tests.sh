024f72a45e8337c510c9278710f642fb9d996d18
Sylvain MARIE <sylvain.marie@se.com>
2020-09-17 14:59:45 +0200

    Fixed coverage report generation on travis

--- ci_tools/run_tests.sh
+++ ci_tools/run_tests.sh
@@ -15,7 +15,7 @@ trap "cleanup" INT TERM EXIT
 if [ "${TRAVIS_PYTHON_VERSION}" = "3.5" ]; then
    # full
    # Run tests with "python -m pytest" to use the correct version of pytest
-   echo -e "\n\n****** Running tests ******\n\n"
+   echo -e "\n\n****** Running tests with coverage ******\n\n"
    coverage run --source doit_api -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html -v doit_api/tests/
    # buggy
    # python -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./doit_api -v doit_api/tests/
